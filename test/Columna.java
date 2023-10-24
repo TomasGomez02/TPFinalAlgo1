97d40bbc4768944afb7996ded2a336c2749c87f6
datascience <varrudi@estudiantes.unsam.edu.ar>
2023-10-24 16:17:28 -0300

    Mi primer commit

--- test/Columna.java
+++ test/Columna.java
@@ -1,6 +1,14 @@
 package test;
 
-public abstract class Columna
-{
-
+public abstract class Columna<T> implements Cloneable {
+    public abstract T getCelda(int indice);
+    public abstract void setCelda(int indice, T valor);
+    public abstract void añadirCelda(int indice, T valor);
+    public abstract void añadirCelda(T valor);
+    public abstract void eliminarCelda(int indice);
+    public abstract void borrarValorCelda(int indice);
+    public abstract Columna<T> recortarColumna(int indiceInicio, int indiceFinal);
+    public abstract void concatenarColumna(Columna<T> otraColumna);
+    public abstract int length();
+    public abstract void ordenar(boolean creciente);
 }
--- test/ColumnaString.java
+++ test/ColumnaString.java
@@ -1,5 +1,91 @@
-package TPFinalAlgo1.test;
+package test;
 
-public class ColumnaString {
+import java.util.ArrayList;
+import java.util.Comparator;
+import java.util.List;
+
+public class ColumnaString extends Columna<String> {
+    private List<String> data;
+
+    public ColumnaString(){
+        this.data = new ArrayList<>();
+    }
+
+    public ColumnaString(List<String> otro){
+        this.data = new ArrayList<>(otro);
+    }
+
+    @Override
+    public String getCelda(int indice) {
+        return this.data.get(indice);
+    }
+
+    @Override
+    public void setCelda(int indice, String valor) {
+        this.data.set(indice, valor);
+    }
+
+    @Override
+    public void añadirCelda(int indice, String valor) {
+        this.data.add(indice, valor);
+    }
+
+    @Override
+    public void añadirCelda(String valor) {
+        this.data.add(valor);
+    }
+
+    @Override
+    public void eliminarCelda(int indice) {
+        this.data.remove(indice);
+    }
+
+    @Override
+    public void borrarValorCelda(int indice) {
+        this.data.set(indice, null);
+    }
+
+    @Override
+    public ColumnaString recortarColumna(int indiceInicio, int indiceFinal) {
+        ColumnaString recorte = new ColumnaString();
+        for (int i = 0; i < this.length(); i++) {
+            if (i >= indiceInicio && i <= indiceFinal){
+                recorte.añadirCelda(this.getCelda(i));
+            }
+        }
+        return recorte;
+    }
+
+    @Override
+    public void concatenarColumna(Columna<String> otraColumna) {
+        // TOMAAASSS: Probar este metodo mas tarde
+        for (int i = 0; i < otraColumna.length(); i++) {
+            this.añadirCelda(otraColumna.getCelda(i));
+        }
+    }
+
+    @Override
+    public int length() {
+        return this.data.size();
+    }
+
+    @Override
+    public void ordenar(boolean creciente) {
+        // TOMAAAAS: hay que probar esto tambien
+        Comparator<String> c;
+        if (creciente){
+            c = (a, b) -> a.compareTo(b);
+        }
+        else{
+            c = (a, b) -> b.compareTo(a);
+        }
+        this.data.sort(c);
+    }
+
+    @Override
+    public ColumnaString clone(){
+        ColumnaString copia = new ColumnaString(this.data);
+        return copia;
+    }
     
 }
